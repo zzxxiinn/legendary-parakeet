edb7dc43a07424613d2f0a44e52873616d9503d4
张鑫 <zhangxin8344@gmail.com>
2022-10-17 07:56:42 +0800

    u

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## reference
 
 - [How To Make a React Component Editor with Live Preview](https://betterprogramming.pub/creating-a-simple-live-react-editor-a6cc17e77beb)
+
+
+
+- [] code and preview online
+- [] copy
+- [] language support
---
+++ src/components/SandBox.jsx
@@ -0,0 +1,57 @@
+import "./sandbox.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createEditor } from "./editor";
+import debounce from "debounce";
+
+// default code
+const code = `import x from 'x';
+
+// edit this example
+
+function Greet() {
+  return <span>Hello World!</span>
+}
+
+<Greet />
+`;
+
+
+export class SandBox extends React.Component {
+    state = {
+        code
+    };
+
+    editor = null;
+
+    el = null;
+
+    componentDidMount() {
+        this.editor = createEditor(this.el);
+        this.editor.run(code);
+    }
+
+    onCodeChange = ({ target: { value } }) => {
+        this.setState({ code: value });
+        this.run(value);
+    };
+
+    run = debounce(() => {
+        const { code } = this.state;
+        this.editor.run(code);
+    }, 500);
+
+    render() {
+        const { code } = this.state;
+        return (
+            <div className="app">
+                <div className="split-view">
+                    <div className="code-editor">
+                        <textarea value={code} onChange={this.onCodeChange} />
+                    </div>
+                    <div className="preview" ref={el => (this.el = el)} />
+                </div>
+            </div>
+        );
+    }
+}
